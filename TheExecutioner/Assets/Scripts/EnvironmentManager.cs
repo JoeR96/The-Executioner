ba74d151daad4bd342ddee736c19eb4c94389607
Joe Richardson <joeyrichardson96@gmail.com>
2021-02-15 20:40:26 +0000

    Level Testing

--- TheExecutioner/Assets/Scripts/EnvironmentManager.cs
+++ TheExecutioner/Assets/Scripts/EnvironmentManager.cs
@@ -1,101 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
+using Unity.Mathematics;
 using UnityEngine;
 
 public class EnvironmentManager : MonoBehaviour
 {
+    public Transform SpawnPoint;
+    public int gridX;
+    public int gridZ;
+    public int y;
+    public float gridSpaceOffset;
+    public GameObject floorContainer;
 
-    private RotationManager _rotationManager;
-
-    // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
-        _rotationManager = GetComponent<RotationManager>();
+        SpawnGrid();
     }
-
-    // Update is called once per frame
-    void Update()
+    protected void SpawnGrid()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
+        for (int x = 0; x < gridX; x++)
         {
-            StartCoroutine(RotatePlatform());
-        }
-        if (Input.GetKeyDown(KeyCode.F2))
-        {
-            ResetPlatform();
-        }
-        if (Input.GetKeyDown(KeyCode.F3))
-        {
-            LowerPlatform();
-        }
-    }
-
-    
-
-    [SerializeField] private Transform[] _rotatingPlatforms;
-    [SerializeField] private Quaternion[] _rotatingPlatformRotations;
-    private float _rotationDuration = 0.1f;
-    
-    private IEnumerator RotatePlatform()
-    {
-        foreach (var platform in _rotatingPlatforms)
-        {
-            var t = platform.GetComponent<ICheckDistanceFromPlayer>();
-            if (!t.PlayerIsInTheArea())
+            for (int z = 0; z < gridZ; z++)
             {
-
-
+                Vector3 spawnPosition = new Vector3(x * gridSpaceOffset,y, z * gridSpaceOffset );
+                SpawnChunk(spawnPosition,quaternion.identity);
             }
-            else
-            {
-                StartCoroutine(StartMovement(platform));
-
-            }
-
-            yield return null;
-        }
-
-    }
-
-    private IEnumerator StartMovement(Transform platform)
-    {
-        StartCoroutine(LerpTransformPosition(platform, new Vector3(platform.position.x,platform.position.y - 2.5f, platform.position.z), _rotationDuration));
-        yield return new WaitForSeconds(0.1f);
-        _rotationManager.RotateGameObject(platform);
-        yield return new WaitForSeconds(0.1f);
-        StartCoroutine(LerpTransformPosition(platform, new Vector3(platform.position.x,platform.position.y + 2.5f, platform.position.z), _rotationDuration));
-    }
-    private void ResetPlatform()
-    {
-        foreach (var platform in _rotatingPlatforms)
-        {
-            StartCoroutine(ResetPlatforms(platform));
         }
     }
 
-    private IEnumerator ResetPlatforms(Transform platform)
+    protected void SpawnChunk(Vector3 positionToSpawn, Quaternion rotationToSpawn)
     {
+        var chunk = Instantiate(floorContainer, positionToSpawn, rotationToSpawn);
+        chunk.transform.SetParent(gameObject.transform);
         
-        yield return new WaitForSeconds(0.1f);
-        platform.GetComponent<RotatingPlatform>().ReturnToOriginalRotation();
-        yield return new WaitForSeconds(0.1f);
-        StartCoroutine(LerpTransformPosition(platform, new Vector3(platform.position.x,platform.position.y + 2.5f, platform.position.z), _rotationDuration));
     }
-    
-    private void LowerPlatform()
-    {
-        foreach (var platform in _rotatingPlatforms)
-        {
-            StartCoroutine(LowerPlatforms(platform));
-        }
-    }
-    private IEnumerator LowerPlatforms(Transform platform)
-    {
-        StartCoroutine(LerpTransformPosition(platform, new Vector3(platform.position.x,platform.position.y - 2.5f, platform.position.z), _rotationDuration));
-        yield return null;
-    }
-
 
     public IEnumerator LerpTransformPosition(Transform transformToLerp, Vector3 target, float duration)
     {
