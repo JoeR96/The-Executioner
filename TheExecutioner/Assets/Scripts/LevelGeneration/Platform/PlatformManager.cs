f91c773e3c2cc88819d37aab62fd7cf86b24fde0
Joe Richardson <joeyrichardson96@gmail.com>
2021-05-07 14:38:48 +0100

    Added properties to platform modules

--- TheExecutioner/Assets/Scripts/LevelGeneration/Platform/PlatformManager.cs
+++ TheExecutioner/Assets/Scripts/LevelGeneration/Platform/PlatformManager.cs
@@ -2,10 +2,20 @@
 
 public class PlatformManager : MonoBehaviour
 {
-    public PlatformRampManager PlatformRampManager;
-    public PlatformBridgeManager PlatformBridgeManager;
-    public PlatformColourManager PlatformColourManager;
-    public PlatformHeightManager PlatformHeightManager;
-    public PlatformSpawnManager PlatformSpawnManager;
-    public PlatformStateManager PlatformStateManager;
+    public PlatformRampManager PlatformRampManager { get; private set; }
+    public PlatformBridgeManager PlatformBridgeManager { get; private set; }
+    public PlatformColourManager PlatformColourManager { get; private set; }
+    public PlatformHeightManager PlatformHeightManager { get; private set; }
+    public PlatformSpawnManager PlatformSpawnManager { get; private set;}
+    public PlatformStateManager PlatformStateManager { get; private set;}
+
+    private void Awake()
+    {
+        PlatformRampManager = GetComponent<PlatformRampManager>();
+        PlatformBridgeManager = GetComponent<PlatformBridgeManager>();
+        PlatformColourManager = GetComponent<PlatformColourManager>();
+        PlatformHeightManager = GetComponent<PlatformHeightManager>();
+        PlatformSpawnManager = GetComponent<PlatformSpawnManager>();
+        PlatformStateManager = GetComponent<PlatformStateManager>();
+    }
 }
